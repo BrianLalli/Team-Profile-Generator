db46d6a376d8a8155b8108ab3c40149a33cb25e7
Brian Lalli <106120015+BrianLalli@users.noreply.github.com>
2022-07-21 20:47:07 -0500

    getting files ready

---
+++ index.js
@@ -0,0 +1,82 @@
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+let responses = [];
+
+//engineer question array
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+    },
+
+]
+
+//intern question array
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school did you attend?'
+    },
+
+]
+
+//manager question array
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: 'What is your office number?'
+    },
+
+]
+
+
