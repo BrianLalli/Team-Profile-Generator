fc7436a58c985cc2edcc67da11cebee1d466d822
Brian Lalli <106120015+BrianLalli@users.noreply.github.com>
2022-07-24 14:48:22 -0500

    added functions for each class

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const generateHTML = require('./lib/generateHTML');
+
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
@@ -80,18 +85,70 @@ const managerQuestions = [
 ]
 
 // Create a main function to ask user what they will want to do
-// Four options: create Manager, Engineer, or Intern, or finish creating
+const chooseNext = [{
+    type: 'list',
+    name: 'choice',
+    message: 'Choose who to add next.',
+    // Four options: create Manager, Engineer, or Intern, or finish creating
+    choices: ['Engineer', 'Intern', 'Finished Creating']
+}]
+
 // Each option needs to have a function
-    // createManager();
-        // ask for Manager info
-            // name
-            // id
-            // email
-            // officeNumber
-                // use answers to create a new Manager object using Manager class
-                    // store new Manager inside of an array
-    // createEngineer();
-    // createIntern();
+   // ask for Manager info
+const init = () => {
+    inquirer.prompt(managerQuestions)
+    .then(answers => {
+    // use answers to create a new Manager object using Manager class
+      const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum);
+      responses.push(manager);
+      console.log(responses);
+      //push to team array (manager parameter)
+      //call function to create new team member
+      nextPerson();
+    })
+  }
+  
+  const chooseNext = () => {
+    inquirer.prompt(chooseNext)
+    .then(answers => {
+      //choice = engineer, run engineer function
+      if(answers.choice === 'Engineer'){
+        makeEngineer();
+      } else if (answers.choice === 'Intern') {
+        makeIntern();
+      } else {
+        generateHTML(responses);
+      }
+    })
+  }
+// createEngineer();
+  const makeEngineer = () => {
+    inquirer.prompt(engineerQuestions)
+    .then(answers => {
+      const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+      responses.push(engineer);
+      console.log(responses);
+      //push to team array (engineer parameter)
+      //call function to create new team member
+      chooseNext();
+    })
+  }
+  
+// createIntern();
+  const makeIntern= () => {
+    inquirer.prompt(internQuestions)
+    .then(answers => {
+      const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+      responses.push(intern);
+      console.log(responses);
+      //push to team array (engineer parameter)
+      //call function to create new team member
+      chooseNext();
+    })
+  }
+    
+     
+
 // Create generate HTML function
     // creating HTML template
     // writing template into a file
